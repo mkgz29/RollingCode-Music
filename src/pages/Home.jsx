3e73b7c32ad38e126ce5e17ddeed27cd1b83af96
Miguel Gomez () <moge22alternativa@gmail.com>
2026-01-13 13:57:06 -0300

    Mover estructura del proyecto a la raíz del repositorio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Rolling Code Music</h1>
+        </div>
+    )
+}
+
+export default Home;
+
+/* Home, funcion flecha que representa la pantalla de la app, App.jsx renderiza Home.jsx 
+
+Home = pantalla principal */
