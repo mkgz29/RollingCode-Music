9c5ba27e2df06dc3088b3442f7587f8287f30be6
Miguel Gomez () <moge22alternativa@gmail.com>
2026-01-19 01:22:17 -0300

    fix: navbar responsive y fondo negro completo

--- src/data/songsStorage.js
+++ src/data/songsStorage.js
@@ -1,10 +1,27 @@
-const keyStorage = "songs";
+const STORAGE_KEY = 'songs';
 
-export function getSongs {
-    const data = LocalStorage.getItem(keyStorage);
-    if (data) {
-    return JSON.parse(data);
-} else {
-    return [];
+export function getSongs() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  return data ? JSON.parse(data) : [];
+}
+
+export function saveSongs(songs) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(songs));
 };
-};
+
+export function addSong(newSong) {
+  const songs = getSongs();
+
+  newSong.id = Date.now();
+  songs.push(newSong);
+
+  saveSongs(songs);
+}
+
+export function deleteSong(id) {
+  const songs = getSongs();
+
+  const updatedSongs = songs.filter(song => song.id !== id);
+
+  saveSongs(updatedSongs);
+}
